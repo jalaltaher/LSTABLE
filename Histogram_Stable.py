d490738484b6a5ffa06b8d40a50559d4d9353e0e
Taher Jalal <147136539+jalaltaher@users.noreply.github.com>
2024-11-25 22:14:54 +0100

    Initial commit

---
+++ Histogram_Stable.py
@@ -0,0 +1,84 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Nov 21 21:39:53 2024
+
+@author: Jalal
+"""
+
+import numpy as np
+import matplotlib.pyplot as plt 
+import scipy.stats as st
+from Stable import*
+
+# =============================================================================
+# Histograms and densities with varying 
+# =============================================================================
+
+
+def histogram_zoom(array: np.ndarray,minimum_bound: float,maximum_bound:float,nb_bins: int):
+    ''' 
+    Computes bins and heights of a normalized histogram 
+    of an array zooming on values that are inside 
+    [infimum_bound, supremum_bound].
+    
+    Parameters: 
+        array: (np.ndarray) array of values
+        infimum_bound: (float) lower bound of the zoom box
+        supremum_bound: (float) upper bound of the zoom box
+        nb_bins: (int) number of bins for the histogram
+    
+    Returns:
+        tuple: (values, bins)
+            values: Heights of the histogram bins in [infimum_bound, supremum_bound].
+            bins: Edges of the histogram bins in [infimum_bound, supremum_bound].
+    '''
+    min_array=np.min(array)
+    max_array=np.max(array)
+    if min_array> minimum_bound:
+        raise ValueError('minimum bound not included in the support of the array')
+    if max_array<maximum_bound:
+        raise ValueError('maximum bound not included in the support of the array')
+        
+    bins=np.linspace(minimum_bound,maximum_bound,nb_bins) 
+    bins=np.concatenate(([min_array],bins,[max_array])) #Adding the bounds as one range value
+    values,bins=np.histogram(array,bins=bins,density=True)
+    return values[1:-1],bins[1:-1] #dropping the bins and values between the extremums and the desired bounds.
+
+
+def plot_stable_density_histogram(alpha: float,sigma:float, beta:float,mu:float,nb_sample: int, plot_grid: np.ndarray, nb_bins: int, density=True, histogram=True):
+    ''' Plot a stable density and the histogram with nb_bins bins of n_sample drawing of S_alpha(sigma,beta,mu) in a given grid
+    
+    Parameters: 
+    
+    nb_sample: (int) number of sample values of S_\alpha(sigma,beta,mu)
+    plot_grid: (np.ndarray) grid of display
+    nb_bins: (int) number of bins for the histogram
+    Density: (bool) 
+    
+    '''
+    #One of the density or the histogram has to appear
+    if density and histogram==False:
+        raise ValueError('One of the density or the histogram have to be plotted')
+    
+    #bound of the plot grid
+    minimum_bound= plot_grid[0]
+    maximum_bound= plot_grid[-1]
+    
+    plt.figure()
+    
+    if density==True:
+        density=stable_density(plot_grid,alpha,sigma,beta,mu)
+        plt.plot(plot_grid,density,label='density')
+    if histogram==True:
+        sample_array=stable_distribution_generator(alpha, sigma, beta, mu, nb_sample)
+        values,bins=histogram_zoom(sample_array, minimum_bound,maximum_bound,nb_bins)
+        plt.stairs(values,bins)
+    plt.show()
+    
+
+
+# alpha,sigma,beta,mu=1,1.0,0.2,0.0
+# nb_sample=10000
+# plot_grid=np.linspace(-3,3,1000)
+# nb_bins=floor(sqrt(nb_sample))
+# plot_stable_density_histogram(alpha,sigma,beta,mu,nb_sample, plot_grid, nb_bins)
---
+++ Stable_Levy_Process.py
@@ -0,0 +1,95 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Nov 25 20:43:55 2024
+
+@author: Jalal
+"""
+
+
+
+import numpy as np
+from scipy.integrate import romb, quad, simpson
+import scipy.stats as st
+import matplotlib.pyplot as plt
+from math import cos, gamma, pi,log,tan,sin,floor,exp,ceil,sqrt
+from scipy.special import sici
+from scipy.integrate import quad
+import scipy.optimize as so
+import mpmath
+from scipy.special import gamma, gammainc, gammaincc,gammaincinv
+import pandas as pd
+import timeit
+from mpmath import gammainc
+import mpmath as mp
+
+
+
+def increment_stable_levy_process_generator(n: int, Delta: float,alpha:float, P: float, Q:float, drift: float, nb_sample: int):
+    '''
+    
+    Generates increments of an alpha stable Levy process with triplet (drift,0,nu) where nu is the stable
+    Levy measure with parameter alpha,P,Q
+    
+    Parameters
+    ----------
+    n : (int)
+        Number of increments
+    Delta : float
+        Time step for the increments
+    alpha : float
+        stability index 0<alpha<2
+    P : float
+        Stable Levy measure parameter for positive jumps
+    Q : float
+        Stable Levy measure parameter for negative jumps
+    drift : float
+        Drift term
+    nb_sample : int
+        Number of trajectories
+
+    Returns (numpy matrix)
+    -------
+    numpy matrix of size (nb_sample,n) each line corresponding the the increments of a trajectory
+
+    '''
+    #Parameter conversion
+    alpha,sigma,beta,mu=stable_to_levy_parameter(alpha, P, Q, drift)
+    
+    #Initial sampling
+    sample_matrix=st.levy_stable(alpha,beta).rvs((nb_sample,n)) #Generate a matrix of S_alpha(1,beta,0)
+    res=np.zeros((nb_sample,n))
+    
+    #Rescaling
+    if alpha!=1:
+        res=sigma*Delta**(1/alpha)*sample_matrix + Delta*mu
+    else:
+        res= sigma*Delta*sample_matrix + Delta*mu + (2/pi)*sigma*Delta*log(sigma*Delta)*beta
+    return res
+    
+
+
+def trajectory_stable_Levy_process_generator(n: int, Delta: float,alpha:float, P: float, Q:float, drift: float, nb_sample: int):
+    '''
+    Generates trajectories of an alpha stable Lévy process with triplet (drift,0,nu) and nu the Lévy measure
+    is characterized bv alpha,P,Q.
+    
+    '''
+    #Increments generation
+    increments_matrix =increment_stable_levy_process_generator(n, Delta,alpha, P, Q, drift, nb_sample)
+    #Add a column of zeros as a first value at t=0
+    increments_matrix_add0 = np.hstack([np.zeros((nb_sample,1)),increments_matrix])
+    
+    return np.cumsum(increments_matrix_add0,axis=1)
+    
+    
+    
+# n,Delta=1000,0.01
+# alpha,P,Q=1,1,1
+# drift=0
+# nb_sample=10
+# time_grid=np.linspace(0,n*Delta,n+1)
+# trajectory = trajectory_stable_Levy_process_generator(n, Delta,alpha, P, Q, drift, nb_sample)
+# plt.figure()
+# plt.plot(time_grid,trajectory.T)
+# plt.show()
+    
